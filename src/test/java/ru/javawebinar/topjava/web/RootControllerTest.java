928c9cc7a3d805412e6861e6acd957d5c1b6f1cb
Il'ya Suhov <menace1290@yandex.ru>
2020-09-09 12:31:09 +0300

    9 09

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -1,10 +1,6 @@
 package ru.javawebinar.topjava.web;
 
-import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
-import ru.javawebinar.topjava.model.User;
-
-import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
